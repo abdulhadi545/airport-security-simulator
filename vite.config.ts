3d9d3108a7acdd75a6bf8d88145b4a3f3796ff08
Abdulhadi Hamid <bh127924@gmail.com>
2026-09-18 11:23:21 +0300

    Configure GitHub Pages deployment path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/airport-security-simulator/",
   server: {
     host: "::",
     port: 8080,
